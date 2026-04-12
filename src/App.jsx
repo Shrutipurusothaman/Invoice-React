ecbb7f9cac5cf69bfa076b0eca1aada78707f5c3
Shrutipurusothaman <shrutikishore04@gmail.com>
2026-04-12 23:55:06 +0530

    UI width reduced

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   
 
   return (
-    <div className='border border-gray-500 mx-20 my-10 rounded-lg'>
+    <div className='border border-gray-500 mx-auto my-10 rounded-lg max-w-4xl'>
       <h1 className='text-center text-2xl m-3 font-semibold'>INVOICE GENERATOR</h1>
       <Client />
       <User />
